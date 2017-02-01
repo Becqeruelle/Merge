5aa84779eb87fa0d4c56ab26568f23a8ebde4ffb
svc-student <svc-student@student.directory.jccc>
2017-02-01 13:02:33 -0600

    initial merge project commit

---
+++ merge.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p "Blank.unity" "Red.unity" "Blue.unity" "End.unity"
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -projectPath "C:\Users\svc-student\Desktop\UnityExample" -buildWindowsPlayer "C:\Users\svc-student\Desktop\Totoro"
+@echo Build successful!
+@pause
